adbbd6749c389316ac0f46575ed9662731376143
Yoav <yoavklein25@gmail.com>
2023-08-14 10:25:37 +0300

    Annotations

---
+++ Annotations/Foo.java
@@ -0,0 +1,14 @@
+
+@ClassPreamble {
+    author = "John doe",
+    date = "1/2/2020",
+    currentRevision = 5,
+    lastModified = "2/4/2022",
+    lastModifiedBy = "Yoav Klein",
+    reviewers = {"Alice", "Bob", "Grisha"}
+}
+public class Foo {
+    void foo() {
+        System.out.println("Foo");
+    }
+}
