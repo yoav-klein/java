68560702b5734c3a0cc0ab288845bfd8e2096b33
Yoav <yoavklein25@gmail.com>
2024-05-24 17:09:29 +0300

    RequestBody

---
+++ Spring/05-spring-mvc/03-handler-methods/app/src/main/webapp/WEB-INF/views/headers.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Headers</title>
+</head>
+<body>
+    <h2>Headers</h2>
+    <p>The header you gave is ${customHeader}</p>
+</body>
+</html>
---
+++ Spring/05-spring-mvc/04-handler-methods-RequestBody/app/src/main/java/spring/mvc/UserController.java
@@ -0,0 +1,20 @@
+package spring.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        // Simulate saving the user to the database
+        String responseMessage = "User created: " + user.getName() + ", Age: " + user.getAge();
+        return new ResponseEntity<>(responseMessage, HttpStatus.CREATED);
+    }
+}
