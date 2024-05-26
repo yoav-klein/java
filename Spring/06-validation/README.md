04d9870a792f508b6e51311a593b60a2904e098e
Yoav <yoavklein25@gmail.com>
2024-05-26 10:22:10 +0300

    Spring Validation

---
+++ Spring/06-validation/README.md
@@ -0,0 +1,6 @@
+# Validation
+---
+
+This example demonstrates the use of Spring's Validator interface.
+
+See [Validation by Using Spring’s Validator Interface](https://docs.spring.io/spring-framework/reference/core/validation/validator.html)
---
+++ Spring/06-validation/app/src/main/java/my/spring/Person.java
@@ -0,0 +1,13 @@
+package my.spring;
+
+public class Person {
+
+	private String name;
+	private int age;
+
+	int getAge() { return age; }
+	String getName() { return name; }
+
+	void setAge(int age) { this.age = age; }
+	void setName(String name) { this.name = name; }
+}
