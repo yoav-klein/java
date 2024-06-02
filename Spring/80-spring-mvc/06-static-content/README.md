55efa4a2399646fb05593952aaa0391c21c38cac
Yoav <yoavklein25@gmail.com>
2024-06-02 21:18:12 +0300

    Static Content with Resource Handlers

---
+++ Spring/80-spring-mvc/06-static-content/README.md
@@ -0,0 +1,5 @@
+# Serving Static Content
+---
+
+Service static content in Spring MVC is done using _Resource Handlers_.
+
---
+++ Spring/80-spring-mvc/06-static-content/app/src/main/java/spring/mvc/MyWebConfig.java
@@ -0,0 +1,34 @@
+
+package spring.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spring.mvc")
+public class MyWebConfig implements WebMvcConfigurer {
+
+	@Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/public", "classpath:/static/");
+            // .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+	}
+
+
+}
