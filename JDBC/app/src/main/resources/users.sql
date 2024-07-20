11fa8d7298014e49622b7727c9ffb0f2eadb0614
Yoav <yoavklein25@gmail.com>
2024-07-20 22:26:54 +0300

    Databases

--- JDBC/app/src/main/resources/users.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists USERS (id identity primary key, name varchar(255) not null);
-truncate table USERS;
-insert into USERS (name) values ('Yoav');
-insert into USERS (name) values ('Dikla');
