3655dfe8a0c09008f52e5ef46ea5b200f86f3186
yoklein <yoav.klein@cyberark.com>
2024-07-31 11:17:07 +0300

    Adding jquery dependency

--- TestNG/01-basic/run.cmd
+++ TestNG/01-basic/run.cmd
@@ -1,7 +1,8 @@
-java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml
 
+java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml
 rem Run only "fast" tests
-java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml -groups fast
+java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml -groups fast
+
 
 rem Run only "slow" tests
-java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml -groups slow
+java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml -groups slow
