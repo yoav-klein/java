a5c9dabd53a04a333fd827eab4de9693206faada
yoklein <yoav.klein@cyberark.com>
2024-07-31 09:31:01 +0300

    fix

--- TestNG/01-basic/run.cmd
+++ TestNG/01-basic/run.cmd
@@ -1,1 +1,7 @@
 java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml
+
+rem Run only "fast" tests
+java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml -groups fast
+
+rem Run only "slow" tests
+java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;./classes org.testng.TestNG testng.xml -groups slow
