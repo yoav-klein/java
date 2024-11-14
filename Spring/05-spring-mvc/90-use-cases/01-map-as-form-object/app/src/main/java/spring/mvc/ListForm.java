38f33cc3e1e1f2b033eec6a92c93fd35dcfff148
Yoav <yoavklein25@gmail.com>
2024-11-14 14:25:11 +0200

    Use cases

--- Spring/05-spring-mvc/90-use-cases/01-map-as-form-object/app/src/main/java/spring/mvc/ListForm.java
+++ Spring/05-spring-mvc/90-use-cases/01-map-as-form-object/app/src/main/java/spring/mvc/ListForm.java
@@ -1,13 +1,16 @@
 package spring.mvc;
 
 import java.util.Map;
+
+import spring.mvc.Category;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListForm {
     
-    private Map<String, List<String>> list;
+    private Map<Category, List<String>> list;
 
     public ListForm() {
         list = new HashMap<>();
@@ -15,24 +18,24 @@ public class ListForm {
         List<String> vegetables = new ArrayList<>();
         vegetables.add("Tomato");
         vegetables.add("Cucumber");
-        list.put("vegetables", vegetables);
+        list.put(Category.VEGETABLES, vegetables);
 
         List<String> fruit = new ArrayList<>();
         fruit.add("Orange");
         fruit.add("Strawberry");
-        list.put("fruit", fruit);
+        list.put(Category.FRUITS, fruit);
     }
 
-    public void setList(Map<String, List<String>> list) {
+    public void setList(Map<Category, List<String>> list) {
         this.list = list;
     }
 
-    public Map<String, List<String>> getList() {
+    public Map<Category, List<String>> getList() {
         return this.list;
     }
 
     public void print() {
-        for(Map.Entry<String,List<String>> entry : this.list.entrySet()) {
+        for(Map.Entry<Category,List<String>> entry : this.list.entrySet()) {
             System.out.println(entry.getKey());
             System.out.println("====");
 
