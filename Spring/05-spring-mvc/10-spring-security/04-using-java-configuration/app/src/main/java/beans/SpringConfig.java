272db30f1b4923bfd104688d7b33f22adc2df4c1
Yoav <yoavklein25@gmail.com>
2025-01-07 13:51:24 +0200

    Java configuration

---
+++ Spring/05-spring-mvc/10-spring-security/04-using-java-configuration/app/src/main/java/beans/SpringConfig.java
@@ -0,0 +1,55 @@
+
+package beans;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import jakarta.servlet.Filter;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity // EnableWebSecurity injects the HttpSecurity bean and probably other beans as well
+public class SpringConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults())
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            );
+        SecurityFilterChain chain = http.build();
+        List<Filter> filters = chain.getFilters();
+        for(Filter filter : filters) {
+            System.out.println(filter.getClass());
+        }
+
+        System.out.println("===================");
+        return chain;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("password")
+                    .roles("USER")
+                    .build();
+            UserDetails admin = User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("password")
+                    .roles("ADMIN", "USER")
+                    .build();
+            return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
