c835af4a3e3b3587efdfda9d664e00506df56bdc
yoklein <yoav.klein@cyberark.com>
2025-01-08 17:30:46 +0200

    Authorization

---
+++ Spring/05-spring-mvc/10-spring-security/06-authorization/app/src/main/java/beans/SpringConfig.java
@@ -0,0 +1,57 @@
+
+package beans;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity // EnableWebSecurity injects the HttpSecurity bean and probably other beans as well
+public class SpringConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(configurer -> {
+                configurer.ignoringRequestMatchers("/**");
+            })
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(configurer -> {
+                configurer.loginPage("/myloginpage").permitAll();
+            })
+            .logout(configurer -> {
+                configurer.logoutUrl("/mylogoutpage").permitAll();
+            })
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+                .requestMatchers("/public/**").anonymous()
+                .requestMatchers("/management").hasRole("ADMIN")
+            );
+
+        SecurityFilterChain chain = http.build();
+        return chain;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("password")
+                    .roles("USER")
+                    .build();
+            UserDetails admin = User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("password")
+                    .roles("ADMIN", "USER")
+                    .build();
+            return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
