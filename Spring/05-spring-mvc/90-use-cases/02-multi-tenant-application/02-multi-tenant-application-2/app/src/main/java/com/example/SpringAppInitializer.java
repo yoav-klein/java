6c602fc1fdb061fc0f5466c40217d94b0630dd14
Yoav <yoavklein25@gmail.com>
2025-01-24 15:11:23 +0200

    Multi tenant app - start

---
+++ Spring/05-spring-mvc/90-use-cases/02-multi-tenant-application/02-multi-tenant-application-2/app/src/main/java/com/example/SpringAppInitializer.java
@@ -0,0 +1,35 @@
+
+package spring.mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.example.business.SpringBusinessConfig;
+import com.example.security.SpringSecurityConfig;
+import com.example.web.SpringWebConfig;
+import jakarta.servlet.Filter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+public class SpringAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { SpringBusinessConfig.class, SpringSecurityConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	public Filter[] getServletFilters() {
+		DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
+		delegatingFilterProxy.setTargetBeanName("springSecurityFilterChain");
+		return new Filter[] { delegatingFilterProxy };
+	}
+}
