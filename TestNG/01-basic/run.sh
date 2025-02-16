5b127bd87cad4d801261bd6362e3db1d7fd6883b
yoklein <yoav.klein@cyberark.com>
2025-02-16 09:17:27 +0000

    Slow and fast

--- TestNG/01-basic/run.sh
+++ TestNG/01-basic/run.sh
@@ -1,8 +1,19 @@
 
-java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml
-#rem Run only "fast" tests
-java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml -groups fast
+
+group=$1
+
+if [ "$group" = "fast" ]; then
+    java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml -groups fast
+fi
+
+if [ "$group" = "slow" ]; then
+    java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml -groups slow
+fi
+
+if [ -z "$group" ]; then
+    java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml
+fi
+# Run only "fast" tests
 
 
-#rem Run only "slow" tests
-java -cp lib/jcommander-1.78.jar:lib/testng-7.5.1.jar:lib/jquery-3.5.1.jar:./classes org.testng.TestNG testng.xml -groups slow
+# Run only "slow" tests
