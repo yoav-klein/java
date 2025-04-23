f92d4efa0182c3571bbdd77eb763a96b167abae2
Yoav <yoavklein25@gmail.com>
2025-04-23 08:21:34 +0300

    Multi-tenant app google auth

--- Spring/05-spring-mvc/90-use-cases/03-multi-tenant-app-google-auth/app/src/main/resources/init.sql
+++ Spring/05-spring-mvc/90-use-cases/03-multi-tenant-app-google-auth/app/src/main/resources/init.sql
@@ -1,17 +1,27 @@
 
-CREATE TABLE user(id VARCHAR(70) PRIMARY KEY,
+CREATE TABLE users(id VARCHAR(70) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tenant (id varchar(50) PRIMARY KEY, 
-    name VARCHAR(50) NOT NULL, owner_id VARCHAR(70) NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES user(id)
+CREATE TABLE tenants(id varchar(50) PRIMARY KEY, 
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tenant_user(tenant_id VARCHAR(50) NOT NULL, user_id VARCHAR(70) NOT NULL,
-    FOREIGN KEY (tenant_id) REFERENCES tenant(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+CREATE TABLE tenant_user(tenant_id VARCHAR(50) NOT NULL, 
+    user_id VARCHAR(70) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    display_name VARCHAR(50),
+    FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE (tenant_id, user_id)
+);
+
+CREATE TABLE invitations(tenant_id VARCHAR(50) NOT NULL,
+    user_id VARCHAR(70) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     UNIQUE (tenant_id, user_id)
 );
 
