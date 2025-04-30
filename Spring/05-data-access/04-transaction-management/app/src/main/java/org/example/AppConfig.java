7a8874ce49464f5f619706e1dfa4debcae132f12
Yoav <yoavklein25@gmail.com>
2025-04-30 19:24:21 +0300

    Template app

---
+++ Spring/05-data-access/04-transaction-management/app/src/main/java/org/example/AppConfig.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+public class AppConfig {
+    
+}
