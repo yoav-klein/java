02b6206e355ed07eae2d941b8d258d000b8f02e7
Yoav <yoavklein25@gmail.com>
2025-05-02 10:22:28 +0300

    Spring MVC with security template

--- Spring/90-templates/spring-mvc-with-security/app/src/main/java/org/example/SpringAppInitializer.java
+++ Spring/90-templates/spring-mvc-with-security/app/src/main/java/org/example/SpringAppInitializer.java
@@ -4,7 +4,10 @@ package org.example;
 import org.example.business.SpringBusinessConfig;
 import org.example.security.SpringSecurityConfig;
 import org.example.web.SpringWebConfig;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import jakarta.servlet.Filter;
 
 public class SpringAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -21,5 +24,12 @@ public class SpringAppInitializer extends AbstractAnnotationConfigDispatcherServ
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+
+	@Override
+	public Filter[] getServletFilters() {
+		DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
+		delegatingFilterProxy.setTargetBeanName("springSecurityFilterChain");
+		return new Filter[] { delegatingFilterProxy };
 	}
 }
