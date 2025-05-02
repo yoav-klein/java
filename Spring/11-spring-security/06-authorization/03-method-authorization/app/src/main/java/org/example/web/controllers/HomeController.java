86a7a7e081ae26e7f2c11c62915242badb4763d2
Yoav <yoavklein25@gmail.com>
2025-05-02 10:26:48 +0300

    Method authorization basics

--- Spring/11-spring-security/06-authorization/03-method-authorization/app/src/main/java/org/example/web/controllers/HomeController.java
+++ Spring/11-spring-security/06-authorization/03-method-authorization/app/src/main/java/org/example/web/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package org.example.web.controllers;
 
+import org.example.business.service.FooService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    FooService fooService;
+
     @RequestMapping("/")
     public String sayHello(Model model) {
-        model.addAttribute("name", "Yaffa");
+        fooService.someImportantMethod();
         return "index"; // This corresponds to the view name
     }
    
