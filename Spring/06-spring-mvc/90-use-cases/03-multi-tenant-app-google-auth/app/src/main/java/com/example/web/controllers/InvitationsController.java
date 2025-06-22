9ddf3ad02fabcba407eb95de27521b160f7dcae0
Yoav <yoavklein25@gmail.com>
2025-06-22 22:44:00 +0300

    Minor

--- Spring/06-spring-mvc/90-use-cases/03-multi-tenant-app-google-auth/app/src/main/java/com/example/web/controllers/InvitationsController.java
+++ Spring/06-spring-mvc/90-use-cases/03-multi-tenant-app-google-auth/app/src/main/java/com/example/web/controllers/InvitationsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.business.service.InvitationService;
-import com.example.business.service.TenantService;
 
 
 @Controller
