f5cb7cf4b2191a6ce90afa2fc4f7f872d273f628
Yoav <yoavklein25@gmail.com>
2025-07-02 23:01:22 +0300

    Lifecycle methods

--- TestNG/07-before-stuff/run.cmd
+++ TestNG/07-before-stuff/run.cmd
@@ -1,8 +1,2 @@
 
 java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml
-rem Run only "fast" tests
-java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml -groups fast
-
-
-rem Run only "slow" tests
-java -cp lib/jcommander-1.78.jar;lib/testng-7.5.1.jar;lib/jquery-3.5.1.jar;./classes org.testng.TestNG testng.xml -groups slow
--- TestNG/07-before-stuff/src/com/example/SimpleTest.java
+++ TestNG/07-before-stuff/src/com/example/SimpleTest.java
@@ -1,19 +1,41 @@
 package com.example;
 
 import org.testng.annotations.*;
+import org.testng.*;
 
 public class SimpleTest {
     private int a = 0;
     
+    // just to see that you can use instance variables and initialize them in BeforeClass
     @BeforeClass
-    public void setUp() {
+    public void beforeClass() {
         System.out.println("BEFORE CLASS");
         a = 42;
     }
 
-    @Test(groups = {"fast"})
-    public void aFastTest() {
+    @BeforeMethod(enabled=false)
+    public void beforeMethod() {
+        System.out.println("BEFORE METHoD");
+    }
+
+    @Test
+    public void success() {
         System.out.println(a);
+    }
+
+    @Test
+    public void failing() {
+        Assert.assertEquals(a, 0);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("AFTER METHOD");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("AFTER CLASS");
     }
 
 
