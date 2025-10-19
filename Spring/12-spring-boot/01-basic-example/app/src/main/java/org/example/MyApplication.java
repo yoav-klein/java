c7f6adc2dd2729d1851d64f6cc1ef7ab2ff67635
Yoav <yoavklein25@gmail.com>
2025-10-19 13:26:14 +0300

    Delay

--- Spring/12-spring-boot/01-basic-example/app/src/main/java/org/example/MyApplication.java
+++ Spring/12-spring-boot/01-basic-example/app/src/main/java/org/example/MyApplication.java
@@ -6,6 +6,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,12 @@ public class MyApplication {
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
+	}
+
+	@RequestMapping("/delay")
+	String delay(@RequestParam("seconds") int seconds) throws InterruptedException {
+		Thread.sleep(seconds * 1000);
+		return "delayed";
 	}
 
 	public static void main(String[] args) {
