172e0009b67f0058a18ce3d1c2e1db04b3b74ef2
Yoav <yoavklein25@gmail.com>
2026-06-09 18:06:44 +0300

    i18n

--- Spring/06-spring-mvc/90-use-cases/04-i18n/app/src/main/java/com/example/web/SpringWebConfig.java
+++ Spring/06-spring-mvc/90-use-cases/04-i18n/app/src/main/java/com/example/web/SpringWebConfig.java
@@ -9,13 +9,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+
 
 @Configuration
 @EnableWebMvc
@@ -43,6 +48,25 @@ public class SpringWebConfig implements WebMvcConfigurer, ApplicationContextAwar
         messageSource.addBasenames("Messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
     @Bean    
---
+++ Spring/06-spring-mvc/90-use-cases/04-i18n/app/src/main/webapp/js/main.js
@@ -0,0 +1,11 @@
+
+
+let messages;
+const prom = fetch('messages');
+prom.then(resp => resp.json()).then(data => messages = data);
+
+const pEl = document.getElementById('message');
+document.getElementById('generate-message').addEventListener('click', () => {
+    console.log('click');
+    pEl.innerText = messages['hello'];
+})
