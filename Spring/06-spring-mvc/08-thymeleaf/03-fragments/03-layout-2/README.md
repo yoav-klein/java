1ee9fa7d5a2bc8f8d2350b46397c9531d9165bd6
Yoav <yoavklein25@gmail.com>
2026-06-10 13:02:01 +0300

    Layout 2

---
+++ Spring/06-spring-mvc/08-thymeleaf/03-fragments/03-layout-2/README.md
@@ -0,0 +1,4 @@
+# MVC Template
+---
+
+This directory contains a skeleton of a Spring MVC application. Whenever you need to start, take this as your basis.
---
+++ Spring/06-spring-mvc/08-thymeleaf/03-fragments/03-layout-2/app/src/main/java/spring/mvc/SpringAppInitializer.java
@@ -0,0 +1,22 @@
+
+package spring.mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
